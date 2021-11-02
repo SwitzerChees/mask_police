a8c6138874fbdccb834c747130f603fdbde3494a
switzerchees <patrickmichel96@yahoo.de>
2021-11-02 23:37:08 +0100

    Optimized training of model and fixed webcam detect for not existing face matches

--- training_generator.py
+++ training_generator.py
@@ -1,18 +1,17 @@
 
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dropout, Dense
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.callbacks import ModelCheckpoint
-from PIL import Image, ImageOps
-import tensorflow as tf
+from PIL import Image
 import face_recognition
 import os
-import numpy as np
+import shutil
 
 
-def create_model():
+def create_model(size=125):
     model = Sequential([
-        Conv2D(100, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+        Conv2D(100, (3, 3), activation='relu', input_shape=(size, size, 3)),
         MaxPooling2D(2, 2),
 
         Conv2D(100, (3, 3), activation='relu'),
@@ -28,7 +27,7 @@ def create_model():
     return model
 
 
-def train_model(training_path, validation_path):
+def train_model(training_path, validation_path, model_path, size=(125, 125)):
     model = create_model()
 
     train_datagen = ImageDataGenerator(rescale=1.0/255,
@@ -42,24 +41,30 @@ def train_model(training_path, validation_path):
 
     train_generator = train_datagen.flow_from_directory(training_path,
                                                         batch_size=10,
-                                                        target_size=(150, 150))
+                                                        target_size=size)
 
     validation_datagen = ImageDataGenerator(rescale=1.0/255)
 
     validation_generator = validation_datagen.flow_from_directory(validation_path,
                                                                   batch_size=10,
-                                                                  target_size=(150, 150))
+                                                                  target_size=size)
 
+    os.makedirs(model_path, exist_ok=True)
+    os.makedirs(os.path.join(model_path, 'checkpoints'), exist_ok=True)
+    checkpoint_filepath = os.path.join(model_path, 'checkpoints')
     checkpoint = ModelCheckpoint(
-        'checkpoints/{epoch:03d}.ckpt', monitor='val_loss', verbose=0, save_best_only=True, mode='auto')
+        checkpoint_filepath, monitor='val_loss', verbose=0, save_best_only=True, mode='auto')
     model.fit(
-        train_generator, epochs=30, validation_data=validation_generator, callbacks=[checkpoint])
+        train_generator, epochs=20, validation_data=validation_generator, callbacks=[checkpoint])
+    model.load_weights(checkpoint_filepath)
     model.save('model/my_model')
 
 
 def split_dataset(face_path, mask_path, training_path, validation_path):
+    recreate_dir(training_path)
+    recreate_dir(validation_path)
     face_images = os.listdir(face_path)
-    split_face = int(len(face_images) * 0.75)
+    split_face = int(len(face_images) * 0.7)
     for face_image in face_images[:split_face]:
         get_face(os.path.join(face_path, face_image),
                  os.path.join(training_path, 'face', face_image))
@@ -68,7 +73,7 @@ def split_dataset(face_path, mask_path, training_path, validation_path):
                  os.path.join(validation_path, 'face', face_image))
 
     mask_images = os.listdir(mask_path)
-    split_mask = int(len(mask_images) * 0.75)
+    split_mask = int(len(mask_images) * 0.7)
     for mask_image in mask_images[:split_mask]:
         get_face(os.path.join(mask_path, mask_image),
                  os.path.join(training_path, 'mask', mask_image))
@@ -77,14 +82,15 @@ def split_dataset(face_path, mask_path, training_path, validation_path):
                  os.path.join(validation_path, 'mask', mask_image))
 
 
-def get_face(orig, target, save=True, resize=False):
-    if resize:
-        face_image_np = Image.open(orig)
-        face_image_np = ImageOps.exif_transpose(face_image_np)
-        face_image_np = face_image_np.resize((600, 800))
-        face_image_np = np.array(face_image_np)
-    else:
-        face_image_np = face_recognition.load_image_file(orig)
+def recreate_dir(dir):
+    if os.path.exists(dir):
+        shutil.rmtree(dir, ignore_errors=False, onerror=None)
+    os.makedirs(os.path.join(dir, 'face'), exist_ok=True)
+    os.makedirs(os.path.join(dir, 'mask'), exist_ok=True)
+
+
+def get_face(orig, target, size=(125, 125)):
+    face_image_np = face_recognition.load_image_file(orig)
     faces = face_recognition.face_locations(
         face_image_np, model='hog')
     for (top, right, bottom, left) in faces[:1]:
@@ -92,39 +98,14 @@ def get_face(orig, target, save=True, resize=False):
     if len(faces) == 0:
         return
     face_img = Image.fromarray(face_image_np)
-    face_img = face_img.resize((150, 150))
-    if save:
-        face_img.save(target)
-    return np.array(face_img)
-
-
-def test_model(model_path, test_path):
-    model = load_model(model_path)
-    cases = []
-    for face_image in os.listdir(os.path.join(test_path, 'face')):
-        image = get_face(os.path.join(test_path, 'face', face_image), '', False, True)
-        if image is None:
-            continue
-        image = tf.expand_dims(image, 0)
-        cases.append({'label': 'face', 'image': image})
-    for mask_image in os.listdir(os.path.join(test_path, 'mask')):
-        image = get_face(os.path.join(test_path, 'mask', mask_image), '', False, True)
-        if image is None:
-            continue
-        image = tf.expand_dims(image, 0)
-        cases.append({'label': 'mask', 'image': image})
-    for case in cases:
-        result = model.predict(case['image'])
-        prediction = 'face' if result[0][0] > result[0][1] else 'mask'
-        print('Label:', case['label'], 'Prediction:', prediction)
-    # loss, acc = model.evaluate(test_images, test_labels, verbose=2)
+    face_img = face_img.resize(size)
+    face_img.save(target)
 
 
 if __name__ == '__main__':
-    training_path = 'model_data/training'
-    validation_path = 'model_data/validation'
-    test_path = 'model_data/test'
-    # split_dataset('images/without_mask', 'images/with_mask',
-    #               training_path, validation_path)
-    # train_model(training_path, validation_path)
-    test_model('model/my_model', test_path)
+    training_path = 'model/data/training'
+    validation_path = 'model/data/validation'
+    model_path = 'model'
+    split_dataset('images/without_mask', 'images/with_mask',
+                  training_path, validation_path)
+    train_model(training_path, validation_path, model_path)
--- webcam_detect.py
+++ webcam_detect.py
@@ -1,3 +1,5 @@
+from tensorflow.keras.models import load_model
+import tensorflow as tf
 import face_recognition
 import cv2
 import numpy as np
@@ -11,17 +13,22 @@ video_capture = cv2.VideoCapture(0)
 known_face_encodings = []
 known_face_names = []
 
+model_path = 'model/my_model'
+model = load_model(model_path)
+face_size = (125, 125)
+
 dir = '../../FFHS_Bilder/'
 
-for filename in os.listdir(dir):
-    if filename.endswith(".jpg") or filename.endswith(".png"):
-        print(filename)
-        print(os.path.join(dir, filename))
-        img = face_recognition.load_image_file(os.path.join(dir, filename))
-        img_encoding = face_recognition.face_encodings(img)[0]
-        # Create arrays of known face encodings and their names
-        known_face_encodings.append(img_encoding)
-        known_face_names.append(filename.split('.')[0])
+if os.path.exists(dir):
+    for filename in os.listdir(dir):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            print(filename)
+            print(os.path.join(dir, filename))
+            img = face_recognition.load_image_file(os.path.join(dir, filename))
+            img_encoding = face_recognition.face_encodings(img)[0]
+            # Create arrays of known face encodings and their names
+            known_face_encodings.append(img_encoding)
+            known_face_names.append(filename.split('.')[0])
 
 
 # Initialize some variables
@@ -43,14 +50,15 @@ while True:
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-        print(face_locations)
+        face_encodings = face_recognition.face_encodings(
+            rgb_small_frame, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
 
             # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            matches = face_recognition.compare_faces(
+                known_face_encodings, face_encoding)
             name = "Unknown"
 
             # # If a match was found in known_face_encodings, just use the first one.
@@ -59,10 +67,12 @@ while True:
             #     name = known_face_names[first_match_index]
 
             # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
+            if len(known_face_encodings) > 0:
+                face_distances = face_recognition.face_distance(
+                    known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
 
             face_names.append(name)
 
@@ -77,15 +87,24 @@ while True:
         left *= 4
 
         faceImage = frame[top:bottom, left:right]
-        cv2.imshow('Image',cv2.resize(faceImage,(128,128))) # This faceImage can be used to throw it against the model :)
 
+        image = tf.expand_dims(cv2.resize(faceImage, face_size), 0)
+        result = model.predict(image)
+        prediction = 'mask' if result[0][0] > result[0][1] else 'face'
+        # Choose color: green means ok and red means not good...
+        if prediction == 'mask':
+            color = (0, 255, 0)
+        else:
+            color = (0, 0, 255)
         # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
 
         # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+        cv2.rectangle(frame, (left, bottom - 35),
+                      (right, bottom), color, cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.7, (0, 0, 0), 1)
+        cv2.putText(frame, name, (left + 6, bottom - 6),
+                    font, 0.7, (0, 0, 0), 1)
 
     # Display the resulting image
     cv2.imshow('Video', frame)
