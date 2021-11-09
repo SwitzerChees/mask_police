7845a3a30ac87042efd9ac5a6483e5ccc716147b
Damian Rüegg <damian.rueegg@gmail.com>
2021-11-09 13:53:14 +0100

    Updated speech_generator

--- speech_generator.py
+++ speech_generator.py
@@ -1,11 +1,23 @@
-from azure.cognitiveservices.speech import AudioDataStream, SpeechConfig, SpeechSynthesizer, SpeechSynthesisOutputFormat, ResultReason
+try:
+    import azure.cognitiveservices.speech as speechsdk
+except ImportError:
+    print("""
+    Importing the Speech SDK for Python failed.
+    Refer to
+    https://docs.microsoft.com/azure/cognitive-services/speech-service/quickstart-text-to-speech-python for
+    installation instructions.
+    """)
+    import sys
 
-speech_config = SpeechConfig(subscription="YOURSUBSCRIPTION", region="westeurope")
-speech_config.set_speech_synthesis_output_format(SpeechSynthesisOutputFormat["Riff24Khz16BitMonoPcm"])
-synthesizer = SpeechSynthesizer(speech_config=speech_config, audio_config=None)
+    sys.exit(1)
+
+speech_key, service_region = "", "westeurope"
 
 
 def generate_output_speech(name):
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+    # speech_config.set_speech_synthesis_output_format(speechsdk.SpeechSynthesisOutputFormat)
+    synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
     ssml_string = "<speak version=\"1.0\""
     ssml_string += " xmlns=\"http://www.w3.org/2001/10/synthesis\""
     ssml_string += " xml:lang=\"de-DE\">"
@@ -13,7 +25,6 @@ def generate_output_speech(name):
     ssml_string += name + ", zieh deine Maske an!"
     ssml_string += "</voice> </speak>"
     result = synthesizer.speak_ssml_async(ssml_string).get()
-    if result.reason == ResultReason.Canceled:
+    if result.reason == speechsdk.ResultReason.Canceled:
         print("Error:" + str(result.cancellation_details))
-    stream = AudioDataStream(result)
-    stream.save_to_wav_file("./file.wav")
+
