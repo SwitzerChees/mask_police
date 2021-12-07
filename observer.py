b008cd1842de431ac8737afd239f99fee5a15907
Damian Rüegg <damian.rueegg@gmail.com>
2021-12-07 15:16:36 +0100

    Removed comments

--- observer.py
+++ observer.py
@@ -9,115 +9,56 @@ import speech_generator
 
 
 class Subject(ABC):
-    """
-    The Subject interface declares a set of methods for managing subscribers.
-    """
 
     @abstractmethod
     def attach(self, observer: Observer) -> None:
-        """
-        Attach an observer to the subject.
-        """
         pass
 
     @abstractmethod
     def detach(self, observer: Observer) -> None:
-        """
-        Detach an observer from the subject.
-        """
         pass
 
     @abstractmethod
     def notify(self) -> None:
-        """
-        Notify all observers about an event.
-        """
         pass
 
 
 class Person(Subject):
-    """
-    The Subject owns some important state and notifies observers when the state
-    changes.
-    """
 
     _name: string = "Unbekannt"
     _has_mask: bool = True
-    """
-    For the sake of simplicity, the Subject's state, essential to all
-    subscribers, is stored in this variable.
-    """
 
     _observers: List[Observer] = []
-    """
-    List of subscribers. In real life, the list of subscribers can be stored
-    more comprehensively (categorized by event type, etc.).
-    """
 
     def attach(self, observer: Observer) -> None:
-        print("Subject: Attached an observer.")
         self._observers.append(observer)
 
     def detach(self, observer: Observer) -> None:
         self._observers.remove(observer)
 
-    """
-    The subscription management methods.
-    """
-
     def notify(self) -> None:
-        """
-        Trigger an update in each subscriber.
-        """
 
-        print("Subject: Notifying observers...")
         for observer in self._observers:
             observer.update(self)
 
     def update_name(self, newname) -> None:
-        """
-        Usually, the subscription logic is only a fraction of what a Subject can
-        really do. Subjects commonly hold some important business logic, that
-        triggers a notification method whenever something important is about to
-        happen (or after it).
-        """
 
-        print("\nSubject: I'm doing something important.")
         if newname != self._name:
             self._name = newname
             self.notify()
     
     def update_mask(self, status) -> None:
-        """
-        Usually, the subscription logic is only a fraction of what a Subject can
-        really do. Subjects commonly hold some important business logic, that
-        triggers a notification method whenever something important is about to
-        happen (or after it).
-        """
 
-        print("\nSubject: I'm doing something important.")
         if status != self._has_mask:
             self._has_mask = status
             self.notify()
 
 
 class Observer(ABC):
-    """
-    The Observer interface declares the update method, used by subjects.
-    """
 
     @abstractmethod
     def update(self, subject: Subject) -> None:
-        """
-        Receive update from subject.
-        """
         pass
-
-
-"""
-Concrete Observers react to the updates issued by the Subject they had been
-attached to.
-"""
 
 
 class MaskObserver(Observer):
